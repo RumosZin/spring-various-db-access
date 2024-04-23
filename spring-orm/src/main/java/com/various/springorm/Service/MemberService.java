9bb8367f70a9e1337a006f45709ba4d97fc19e61
rumos <gyunjin3@gmail.com>
2024-04-23 22:14:05 +0900

    [3. 백엔드 개발] 서비스 개발

---
+++ spring-orm/src/main/java/com/various/springorm/Service/MemberService.java
@@ -0,0 +1,33 @@
+package com.various.springorm.Service;
+
+import com.various.springorm.Domain.Member;
+import com.various.springorm.Repository.MemberRepository;
+import com.various.springorm.Repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new
+            MemoryMemberRepository();
+    /**
+     * 회원가입 */
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증 memberRepository.save(member);
+        return member.getId();
+    }
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                }); }
+
+    /**
+     * 전체 회원 조회 */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
