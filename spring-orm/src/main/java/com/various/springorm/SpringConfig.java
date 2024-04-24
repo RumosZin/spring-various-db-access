35c5f0ae873e76cd4a642994ecabd49060c36b5b
rumos <gyunjin3@gmail.com>
2024-04-24 23:54:16 +0900

    [6. 스프링 DB 접근 기술] JDBC 리포지토리 구현

--- spring-orm/src/main/java/com/various/springorm/SpringConfig.java
+++ spring-orm/src/main/java/com/various/springorm/SpringConfig.java
@@ -1,19 +1,30 @@
 package com.various.springorm;
 
+import com.various.springorm.Repository.JDBCMemberRepository;
 import com.various.springorm.Repository.MemberRepository;
 import com.various.springorm.Repository.MemoryMemberRepository;
 import com.various.springorm.Service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JDBCMemberRepository(dataSource);
     }
 }
