b13e2029175645945d3d274ba7843b198b928a54
rumos <gyunjin3@gmail.com>
2024-04-25 19:41:01 +0900

    [6. 스프링 DB 접근 기술] JDBC / JDBCTemplate / JPA / Spring Data JPA 설정 관련 주석 달기

--- spring-orm/src/main/java/com/various/springorm/SpringConfig.java
+++ spring-orm/src/main/java/com/various/springorm/SpringConfig.java
@@ -11,23 +11,25 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    // private final DataSource dataSource; // for JDBC, JDBCTemplate, Spring JPA
+    // private final EntityManager em; // for Spring JPA
+    private final MemberRepository memberRepository; // form Spring Data JPA
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JDBCMemberRepository(dataSource);
-        // return new JDBCTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+
+    // Not necessary in Spring Data JPA, automatically registered Spring Bean
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // return new MemoryMemberRepository();
+//        // return new JDBCMemberRepository(dataSource);
+//        // return new JDBCTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }
